564a2473c98c672419c23428ba1254fffb9ec920
Clofixs <dsnetvork3@gmail.com>
2025-03-25 21:53:22 +0100

    Поменял код для страниц

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Пример: обработка нажатия на кнопку "Начать игру"
 document.getElementById('startGameButton').addEventListener('click', function() {
     alert("Игра начинается! Добро пожаловать в LineFarm!");
     // Здесь можно добавить логику для старта игры
