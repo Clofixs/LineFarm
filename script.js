edf95fde433dc98c96508d5684b169737f023a2b
Clofixs <dsnetvork3@gmail.com>
2025-03-25 22:32:41 +0100

    Замена кода 3

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function init() {
     
     // Создаем рендерер и добавляем его в HTML-страницу
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);  // Делаем рендер большой, по всему экрану
     document.getElementById('game-container').appendChild(renderer.domElement);
 
     // Добавляем свет
