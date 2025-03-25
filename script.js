93d7c4b3331ecc14b950e406e736d2d3db559c5d
Clofixs <dsnetvork3@gmail.com>
2025-03-25 21:36:05 +0100

    Добавлены файлы для сайта

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('registerButton').addEventListener('click', function() {
+    alert("Регистрация начнётся скоро!");
+});
+
+document.getElementById('playButton').addEventListener('click', function() {
+    alert("Игра скоро будет доступна!");
+});
